261e4f618a739f513197f8f5b4433e2bfb273dc6
luanbernuy <lbernuyj1@upao.edu.pe>
2023-05-11 23:59:37 -0500

    f:Creation de la interface TarjetaRepository, para guardar metodos.

--- api-upao-transporte/src/main/java/com/upao/transporte/repository/TarjetaRepository.java
+++ api-upao-transporte/src/main/java/com/upao/transporte/repository/TarjetaRepository.java
@@ -1,5 +1,9 @@
 package com.upao.transporte.repository;
 
-public interface TarjetaRepository {
+import com.upao.transporte.entity.Tarjeta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarjetaRepository extends JpaRepository<Tarjeta,Long> {
+    
 
 }
