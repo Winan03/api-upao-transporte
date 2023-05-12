15a6eca8b90d868c2852c9c95d49dd17659b1f68
luanbernuy <lbernuyj1@upao.edu.pe>
2023-05-12 00:17:34 -0500

    f:Actualizacion de la clase TarjetaServiceImpl(Inyeccion de dependencias.)y metofo create.

--- api-upao-transporte/src/main/java/com/upao/transporte/service/TarjetaServiceImpl.java
+++ api-upao-transporte/src/main/java/com/upao/transporte/service/TarjetaServiceImpl.java
@@ -1,13 +1,20 @@
 package com.upao.transporte.service;
 
 import com.upao.transporte.entity.Tarjeta;
+import com.upao.transporte.repository.TarjetaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TarjetaServiceImpl implements TarjetaService{
 
+    private final TarjetaRepository tarjetaRepository;
+    //Inyeccion de dependencias
+    public TarjetaServiceImpl(TarjetaRepository tarjetaRepository){
+        this.tarjetaRepository= tarjetaRepository;
+    }
+
     @Override
     public Tarjeta createTarjeta(Tarjeta tarjeta) {
-        return null;
+        return tarjetaRepository.save(tarjeta);
     }
 }
