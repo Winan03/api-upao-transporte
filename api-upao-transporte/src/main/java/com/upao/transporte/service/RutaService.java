f903f7c4ad90d94e383e9761556153ac5475a4bf
Sagredag <sagredag2@upao.edu.pe>
2023-05-18 19:40:43 -0500

    Implementación de la interfaz RutaService

---
+++ api-upao-transporte/src/main/java/com/upao/transporte/service/RutaService.java
@@ -0,0 +1,15 @@
+package com.upao.transporte.service;
+
+import com.upao.transporte.entity.RutaDeTransporte;
+
+import java.util.Optional;
+
+public interface RutaService {
+
+    RutaDeTransporte createRutaDeTransporte (RutaDeTransporte ruta);
+    RutaDeTransporte modificarRutaDeTransporte(RutaDeTransporte ruta);
+    Optional<RutaDeTransporte> findByOrigen(String origen);
+    RutaDeTransporte consultarRutaDeTransporte(Long id);
+    void eliminarRutaDeTransporte(Long id);
+
+}
